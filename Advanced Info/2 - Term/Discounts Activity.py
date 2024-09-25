0a6d9f120746dec617fe84d3ac7b6c7e8cd0a7f9
FryPots <martinezserafingilbertoisai@gmail.com>
2024-09-24 20:36:14 -0600

    Finished

--- Advanced Info/2 - Term/Discounts Activity.py
+++ Advanced Info/2 - Term/Discounts Activity.py
@@ -2,33 +2,42 @@ from time import localtime
 
 SALES_TAX = 6
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]
+dis_days = days[1], days[2]
+
+print(dis_days)
 
 def main():
-    
-
     
     def getDiscout(total, discount):
         discount = (total * discount)/100
         return discount
     
-    cos_subtotal = float(input("Type Costumer's Subtotal: $"))
+    cos_input = ""
+    cos_input = input("Type Costumer's Subtotal: $")
+    if cos_input == "0":
+        return False
+    
+    cos_subtotal = float(cos_input)
     
     def getDay():
-        time = localtime()
-        weekday = time.tm_wday
-        return weekday
+        day = localtime().tm_wday
+        return day
     
     day = getDay()
     weekday = days[day]
     date = localtime().tm_mday    
 
     cos_tax = getDiscout(cos_subtotal, SALES_TAX)
-    
-    if weekday in days[1:3]:
+
+    cos_discount = 0
+    if weekday in dis_days:
+        dis_valid = 1
         if cos_subtotal >= 50:
+            dis_valid = 2
             cos_discount = getDiscout(cos_subtotal, 10)
     else:
-        cos_discount = 0
+        dis_valid = 0
+
             
     cos_total = cos_subtotal - cos_discount + cos_tax
     
@@ -46,7 +55,14 @@ Receipt:
     """)
     
     printRecepit()
+    if dis_valid == 1:
+        print(f"""
+Today is discount day, get 10% offsale with a subtotal of $50 or above.
+You still have ${50-cos_subtotal} to go.
+""")
+        
+    return True
         
         
-while True:
+while main() == True:
     main()
