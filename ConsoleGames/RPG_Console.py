946c0f1db6b17b3a48783da79334d3327a5e7418
FryPots <martinezserafingilbertoisai@gmail.com>
2024-09-25 14:21:18 -0600

    Screen Refresh added

--- ConsoleGames/RPG_Console.py
+++ ConsoleGames/RPG_Console.py
@@ -1,28 +1,55 @@
+import os
+import subprocess
+
+
 player_x = 4
 player_y = 4
-player = ["🟩",[player_x, player_y]]
+player = ["🧒",[player_x, player_y]]
 player_coords = player[1]
 player_skin = player[0]
 
-levels = [["⬜⬜⬜⬜⬜⬜⬜",
-           "⬜⬛⬛⬛⬛⬛⬜",
-           "⬜⬛⬜⬛⬛⬛⬜",
-           "⬜⬛⬛⬛⬜⬛⬜",
-           "⬜⬜⬛⬛⬛⬛⬜",
-           "⬜⬜⬛⬛⬛⬜⬜",
-           "⬜⬜⬜⬜⬜⬜⬜"]]
+levels = [["🌳🌳🌳🌳🌳🌳🌳",
+           "🌳🟩🟩🟩🟩🟩🌳",
+           "🌳🟩🌳🟩🟩🟩🌳",
+           "🌳🟩🟩🟩🌳🟩🌳",
+           "🌳🌳🟩🟩🟩🟩🌳",
+           "🌳🌳🟩🟩🟩🌳🌳",
+           "🌳🌳🌳🌳🌳🌳🌳"]]
 
-level = levels[0]
-tile_y = 0
-for line in level:
-    print_line = ""
-    tile_x = 0
-    for l in range(len(line)):
-        if tile_y == player_coords[1] and tile_x == player_coords[0]:
-            print_line += player_skin
-        else:
-            print_line += line[l]
-        tile_x += 1
-    tile_y += 1
-    print(print_line)
+def draw(n):
+    os.system("cls")
+    level = levels[n]
+    screen = ""
+    tile_y = 0
+    for line in level:
+        print_line = ""
+        tile_x = 0
+        for l in range(len(line)):
+            if tile_y == player_coords[1] and tile_x == player_coords[0]:
+                print_line += player_skin
+            else:
+                print_line += line[l]
+            tile_x += 1
+        tile_y += 1
+        screen += print_line + "\n"
+    print(screen)
         
+def player_tick(move):
+    global player_x, player_y, player_skin
+    if move == "w":
+        player_y -= 1
+    if move == "a":
+        player_x -= 1
+    if move == "s":
+        player_y += 1
+    if move == "d":
+        player_x += 1
+    player_coords[0] = player_x
+    player_coords[1] = player_y
+        
+def main():
+    draw(0)
+    player_tick(input())
+    
+while True:
+    main()
