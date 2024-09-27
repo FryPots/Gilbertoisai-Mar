0e9c73589af7810b67d5c04e80a0dcd4eae8896b
FryPots <martinezserafingilbertoisai@gmail.com>
2024-09-26 20:16:15 -0600

    New Assignment Finished

--- Advanced Info/2 - Term/Discounts Activity.py
+++ Advanced Info/2 - Term/Discounts Activity.py
@@ -1,29 +1,52 @@
-import os
 from time import localtime
 
 SALES_TAX = 6
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]
 
-cos_subtotal = int(input("Type Costumer's Subtotal: $"))
+def main():
+    
 
-def getDay():
-    time = localtime()
-    weekday = time.tm_wday
-    return weekday
+    
+    def getDiscout(total, discount):
+        discount = (total * discount)/100
+        return discount
+    
+    cos_subtotal = float(input("Type Costumer's Subtotal: $"))
+    
+    def getDay():
+        time = localtime()
+        weekday = time.tm_wday
+        return weekday
+    
+    day = getDay()
+    weekday = days[day]
+    date = localtime().tm_mday    
 
-day = getDay()
-
-def getDiscout(total, discount):
-    discount = (total * discount)/100
-    return discount
-
-if cos_subtotal >= 50:
-    cos_subtotal -= getDiscout(cos_subtotal, 10)
-
-
-cos_tax = getDiscout(cos_subtotal, 6)
-cos_total = cos_subtotal + cos_tax
-
-print(f"""
-${cos_total} {days[day]}
-""")
+    cos_tax = getDiscout(cos_subtotal, SALES_TAX)
+    
+    if weekday in days[1:3]:
+        if cos_subtotal >= 50:
+            cos_discount = getDiscout(cos_subtotal, 10)
+    else:
+        cos_discount = 0
+            
+    cos_total = cos_subtotal - cos_discount + cos_tax
+    
+    def printRecepit():
+        
+        print(f"""
+{weekday} {date}
+-------------------------------
+Receipt:
+  Subtotal  =   {cos_subtotal}
+  Discount  =   {cos_discount}
+  Sales_Tax =   {cos_tax}
+                ---------------
+    Total:      {cos_total}
+    """)
+    
+    printRecepit()
+        
+        
+while True:
+    main()
