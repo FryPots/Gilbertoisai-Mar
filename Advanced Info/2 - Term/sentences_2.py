bc7c65402adae65771945101317765b142caeb18
FryPots <martinezserafingilbertoisai@gmail.com>
2024-10-09 09:14:40 -0600

    now it generates sentences based on the plurality and tense

--- Advanced Info/2 - Term/sentences_2.py
+++ Advanced Info/2 - Term/sentences_2.py
@@ -20,26 +20,26 @@ def get_verb(tense = 0):
     
     return random.choice(words)
 
-def get_preposition(quantity = 1):
+def get_preposition():
     words = ["above","across","against","along","among","around","at","before","behind","below","beneath","beside","between","beyond","by","for","from","in","inside","into","near","of","off","on","out","over","through","to","toward","under","until","with","within","without"]
     return random.choice(words)
 
-def Main():
-    
-    for i in range(0,6):
-        sentence_quantity = random.randint(1, 2)
-        sentence_time     = random.randint(0,2)
+def create_sentence(sentence_quantity = 1, sentence_time = 0):
 
-        sen_determiner      =   (get_determiner(sentence_quantity))
-        sen_obj             =         (get_noun(sentence_quantity))
-        sen_tense           =             (get_verb(sentence_time))
+    sen_determiner      =         (get_determiner(sentence_quantity))
+    sen_obj             =               (get_noun(sentence_quantity))
+    sen_tense           =                   (get_verb(sentence_time))
+            
+    sen_preposition     =   (f"{get_preposition()} {get_determiner()} {get_noun()}")
         
-        sentence_quantity = random.randint(1, 2)
-        sentence_time     = random.randint(0,2)
-        
-        sen_preposition     =   (f"{get_preposition()} {get_determiner()} {get_noun()}")
-    
-        print(f"{sen_determiner.capitalize()} {sen_obj} {sen_tense} {sen_preposition}")
+    print(f"{sen_determiner.capitalize()} {sen_obj} {sen_tense} {sen_preposition}")
+
+def Main():
+    for i in range(0,3):
+        create_sentence(1,i)
+    for i in range(0,3):
+        create_sentence(2,i)
+
         
         
     user = input("generate more?: ")
