16be87fee0709cb2c78b2028ec62c8e7f814577f
FryPots <martinezserafingilbertoisai@gmail.com>
2024-10-17 09:30:34 -0600

    finished

--- Advanced Info/2 - Term/water_flow.py
+++ Advanced Info/2 - Term/water_flow.py
@@ -6,9 +6,19 @@ def water_column_height(tower_height, tank_height): # Calculate the height of th
     return out
 
 def pressure_gain_from_water_height(column_hight):
-    density = 998.2 # kg/meter^2
+    density = 998.2 # kg/meter^3
     g = 9.80665 # meter/second^2
     h = float(column_hight) # meters
     out = density * g * h 
     out = out/1000
+    return out
+
+def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+    f = friction_factor
+    p = 998.2 # kg/meter^2
+    l = pipe_length #in meters
+    d = pipe_diameter * 2000 #in meters
+    v = fluid_velocity**2
+    out = -(f*l*p*v)
+    out = out/d
     return out
