e856287a4ea38882b4a0623af557c689664180a0
FryPots <martinezserafingilbertoisai@gmail.com>
2024-10-21 13:30:58 -0600

    asdfghjgkggfdsd

--- Console Game/RPG test.py
+++ Console Game/RPG test.py
@@ -4,60 +4,69 @@ from Colors import reset, bg_blue, bg_gray, bg_green, bg_red
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-GRID_SIZE = 17
+GRID_SIZE = 15
 player_pos = 18
 tileset = [f"{bg_red}🟫", f"{bg_green}  ", "🌳", f"{bg_gray}  ", "⛺", f"{bg_blue}🟦"]
+collidable = [tileset[2], tileset[3], tileset[5]]
+
 
 def add(tile = 1, times = GRID_SIZE):
-    tile = str(tile)
-    out = ""
-    for i in range(times):
-        out += tile
+    out = [tile, times]
     return out
 
 def make_scene():
-    grid = ""
-    grid += add(2)
+    grid = []
+    grid.append(add(2))
     for i in range(GRID_SIZE - 2):
-        grid += add(2, 1)
-        grid += add(1, GRID_SIZE - 2)
-        grid += add(2, 1)
-    grid += add(2)
+        grid.append(add(2, 1))
+        grid.append(add(1, GRID_SIZE - 2))
+        grid.append(add(2, 1))
+    grid.append(add(2))
     return grid
 
 def player_tick():
     global player_pos
     key = input()
     
-    match key:
-        case "w":
-            if level[player_pos - GRID_SIZE] != "2":
-                player_pos -= GRID_SIZE
-        case "a":
-            if level[player_pos - 1] != "2":
-                player_pos -= 1
-        case "s":
-            if level[player_pos + GRID_SIZE] != "2":
-                player_pos += GRID_SIZE
-        case "d":
-            if level[player_pos + 1] != "2":
-                player_pos += 1
+    def control(char):   
+        if char in "wasd":
+                match char:
+                    case "w":
+                        move_to = player_pos - GRID_SIZE
+                    case "a":
+                        move_to = player_pos - 1
+                    case "s":
+                        move_to = player_pos + GRID_SIZE
+                    case "d":
+                        move_to = player_pos + 1
+                    case _:
+                        move_to = player_pos
+                return move_to
+        
+    def collition(enabled = 1):
+        out = control(key)
+        if enabled == 1:
+            check_tile = int(level[out])
+            if tileset[check_tile] in collidable:
+                return player_pos
+        return out
+    
+    # player_pos = collition()
             
     
 def draw_scene():
     clear()
     out = reset
-    for index in range(len(level)):
-        tile_idx = int(level[index])
-        if index != 0:
-            if index % GRID_SIZE == 0:
-                out += "\n"            
-        if index != player_pos:
-            out += f"{tileset[tile_idx]}{reset}"
-            continue
-        out += " i"
+    for group in level:
+        tile = group[0]
+        times = group[1]
+        for i in range(times):
+            if (i + 1) % (GRID_SIZE + 1) == 0:
+                out += "\n"
+            else:
+                out += tileset[tile]
     print(out)
-
+        
 level = make_scene()
 
 while True:    
