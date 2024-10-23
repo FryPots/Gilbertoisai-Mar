5adff68efe5195198fa1c25a37db10c307bc943d
FryPots <martinezserafingilbertoisai@gmail.com>
2024-10-22 21:30:48 -0600

    fdsgfhdjhgku

--- Console Game/RPG test.py
+++ Console Game/RPG test.py
@@ -2,21 +2,21 @@ import os
 from Colors import reset, bg_blue, bg_gray, bg_green, bg_red
 
 def clear():
+    print(reset)
     os.system('cls' if os.name == 'nt' else 'clear')
 
 GRID_SIZE = 17
+cheat = 0
+brush = 0
 player_pos = 18
 tileset = [f"{bg_red}🟫", f"{bg_green}  ", "🌳", f"{bg_gray}  ", "⛺", f"{bg_blue}🟦"]
 
 def add(tile = 1, times = GRID_SIZE):
-    tile = str(tile)
-    out = ""
-    for i in range(times):
-        out += tile
+    out = [[tile, times]]
     return out
 
 def make_scene():
-    grid = ""
+    grid = []
     grid += add(2)
     for i in range(GRID_SIZE - 2):
         grid += add(2, 1)
@@ -26,40 +26,80 @@ def make_scene():
     return grid
 
 def player_tick():
-    global player_pos
+    global player_pos, cheat, brush, player_sprite
     key = input()
+    match cheat:
+        case 0:
+            player_sprite = " i"
+        case 1:
+            player_sprite = tileset[brush]
     
+    def isChar(string, position, c = "2"):
+        if position > len(string):
+            return False
+        if string[position] == c:
+            return True
+        return False
+        
     match key:
         case "w":
-            if level[player_pos - GRID_SIZE] != "2":
-                player_pos -= GRID_SIZE
+                act = (player_pos - GRID_SIZE)
         case "a":
-            if level[player_pos - 1] != "2":
-                player_pos -= 1
+                act = (player_pos - 1)
         case "s":
-            if level[player_pos + GRID_SIZE] != "2":
-                player_pos += GRID_SIZE
+                act = (player_pos + GRID_SIZE)
         case "d":
-            if level[player_pos + 1] != "2":
-                player_pos += 1
-            
+                act = (player_pos + 1)
+        case "cheat":
+            cheat = (cheat + 1) % 2
+            input(cheat)
+        case _:
+            return None
+        
+    if key in "wasd":
+        match isChar(level_data, act):
+            case True:
+                if cheat == 1:
+                    player_pos = act
+            case False:
+                player_pos = act
+        
     
 def draw_scene():
+    global level_data
+    def read_data():
+        out = ""
+        tile_idx = 0
+        for group in level:
+            tile = str(group[0])
+            times = (group[1])
+            added = ""
+            for i in range(times):
+                if tile_idx == player_pos:
+                    added += "p"
+                else:
+                    added += tile
+                tile_idx += 1
+            out += added
+        return(out)
     clear()
-    out = reset
-    for index in range(len(level)):
-        tile_idx = int(level[index])
-        if index != 0:
-            if index % GRID_SIZE == 0:
-                out += "\n"            
-        if index != player_pos:
-            out += f"{tileset[tile_idx]}{reset}"
-            continue
-        out += " i"
+    level_data = read_data()
+    out = ""
+    tile_idx = 0
+    for c in level_data:
+        if (tile_idx % GRID_SIZE) == 0:
+            out += "\n"
+        if c.isnumeric():
+            out += tileset[int(c)]
+        if c == "p":
+            out += player_sprite
+        out += reset
+        tile_idx += 1
     print(out)
-
+            
 level = make_scene()
 
-while True:    
+while True:
+    player_tick()   
     draw_scene()
-    player_tick()
+    
