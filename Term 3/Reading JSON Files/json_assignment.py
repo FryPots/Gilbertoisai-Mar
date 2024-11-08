caaa7ea3d968785ce0ecb6b27a536f334d19fd26
FryPots <martinezserafingilbertoisai@gmail.com>
2024-11-08 11:24:00 -0600

    ghfiuyopiuytr

--- Term 3/Reading JSON Files/json_assignment.py
+++ Term 3/Reading JSON Files/json_assignment.py
@@ -2,9 +2,26 @@ import json
 
 import json.scanner
 
-with open(file="data.json", mode="r", newline="") as json_file:
-    data = json.load(json_file)
-    json_file.close()
-students: dict = data
+def read_json(file_name):
+    with open(file=file_name, mode="r", newline="") as json_file:
+        data = json.load(json_file)
+        json_file.close()
+    return data
 
-print(students)
+def convert_json(data):
+    dictionary: dict = {}
+
+    for row in data:
+        values = data[row]
+        keys = values[0]
+        for x in keys:
+            if x not in dictionary:
+                dictionary[x] = []
+            dictionary[x].append(keys[x])
+    return dictionary
+
+if __name__ == "__main__":
+    students = read_json("data.json")
+    for i in students:
+        students[i] = students[i][0]
+    print(students)
